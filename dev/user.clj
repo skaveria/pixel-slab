beb54e3740688c1167570410553cf81223cab923
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-16 15:17:04 -0600

    updated user to have deploy

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,10 @@
 (ns user
   (:require
    [clojure.java.shell :as sh]
+   [clojure.string :as str]
    [clojure.tools.namespace.repl :refer [refresh]]
-   [pixel-slab.core :as slab]))
+   [pixel-slab.core :as slab]
+   [pixel-slab.web :as web]))
 
 (defn go []
   (slab/start!))
@@ -14,21 +16,69 @@
   ([text] (slab/say! text))
   ([opts text] (slab/say! opts text)))
 
+(defn- sh-ok
+  "Run a command and return {:ok? .. :out .. :err .. :exit ..}."
+  [& args]
+  (let [{:keys [exit out err]} (apply sh/sh args)]
+    {:ok? (zero? exit)
+     :exit exit
+     :out (or out "")
+     :err (or err "")}))
+
+(defn git-rev []
+  (let [{:keys [out]} (sh-ok "git" "rev-parse" "--short" "HEAD")]
+    (-> out str/trim)))
+
+(defn git-status []
+  ;; "clean" or "dirty"
+  (let [{:keys [out]} (sh-ok "sh" "-lc" "git status --porcelain | head -n1")]
+    (if (seq (str/trim out)) "dirty" "clean")))
+
 (defn git-pull []
   (println "Pulling latest changes...")
-  (let [{:keys [exit out err]} (sh/sh "git" "pull")]
-    (when (seq out) (println out))
-    (when (seq err) (println err))
-    (if (zero? exit)
+  (let [res (sh-ok "git" "pull")]
+    (when (seq (:out res)) (print (:out res)))
+    (when (seq (:err res)) (print (:err res)))
+    (if (:ok? res)
       (println "Pull complete.")
-      (println "Git pull failed.")))
-  :done)
+      (println "Git pull failed."))
+    (assoc res :rev (git-rev) :state (git-status))))
 
-(defn reload []
+(defn slab-reload []
   (println "Reloading namespaces...")
   (refresh))
 
 (defn slab-update []
   (git-pull)
-  (reload)
+  (slab-reload)
   :updated)
+
+(defn deploy
+  "Deploy the latest code to the slab runtime.
+  Steps:
+  1) git pull
+  2) tools.namespace refresh
+  3) optionally restart the web server if it's running
+  4) speak a short confirmation
+  Returns a summary map."
+  []
+  (let [pull (git-pull)
+        _    (slab-reload)
+        ;; If the web server is running, restart it to pick up handler/template changes cleanly.
+        ;; Safe even if no changes.
+        web-was-running? (web/running?)
+        _    (when web-was-running?
+               (println "Restarting web server...")
+               (web/stop!)
+               (go))
+        rev  (:rev pull)
+        state (:state pull)
+        msg  (str "Deployed " (if (seq rev) rev "latest") ". " state ".")]
+    (println msg)
+    (try
+      (say {:rate 0.95 :pitch 1.05} msg)
+      (catch Exception _ nil))
+    {:deployed true
+     :rev rev
+     :git-state state
+     :web-restarted? web-was-running?}))
