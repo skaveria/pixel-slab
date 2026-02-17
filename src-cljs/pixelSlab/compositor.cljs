7dc57c53d0c97c5717a2e03130bbdf32940fe15a
Nichole Kernreicht <nicholekernreicht@gmail.com>
2026-02-17 14:14:03 -0600

    updated logo

--- src-cljs/pixelSlab/compositor.cljs
+++ src-cljs/pixelSlab/compositor.cljs
@@ -355,6 +355,43 @@
             (when (seq klass)
               (.. vdiv -classList (add klass)))))))))
 
+(defn- set-css-var! [k v]
+  (.. js/document -documentElement -style (setProperty k (str v))))
+
+(defn- ->hue-kw [x]
+  (cond
+    (keyword? x) x
+    (string? x)  (keyword (str/lower-case x))
+    :else        nil))
+
+(defn fetch-theme! []
+  (let [proto (.-protocol js/location)]
+    (cond
+      (= proto "file:")
+      (js/Promise.resolve nil)
+
+      :else
+      (-> (js/fetch "/theme.edn" (clj->js {:cache "no-store"}))
+          (.then (fn [res] (if (.-ok res) (.text res) nil)))
+          (.then (fn [txt]
+                   (when (and txt (seq (str/trim txt)))
+                     (let [m   (reader/read-string txt)
+                           h   (:header m)
+                           hue (->hue-kw (:hue h))
+                           hdr-color (case hue
+                                       :amber "#f6c177"
+                                       :ice   "#a8d8ff"
+                                       :white "#e8e3d6"
+                                       :green "#a6e22e"
+                                       "#a6e22e")]
+                       (set-css-var! "--hdr-color" hdr-color)
+                       (when-let [w (:text-weight h)]    (set-css-var! "--hdr-weight" w))
+                       (when-let [g (:glow h)]           (set-css-var! "--hdr-glow-a" g))
+                       (when-let [g2 (:glow2 h)]         (set-css-var! "--hdr-glow-b" g2))
+                       (when-let [cg (:cursor-glow h)]   (set-css-var! "--cursor-glow-a" cg))
+                       (when-let [cg2 (:cursor-glow2 h)] (set-css-var! "--cursor-glow-b" cg2))))))
+          (.catch (fn [_] nil))))))
+
 ;; ------------------------------------------------------------
 ;; State fetch
 ;; ------------------------------------------------------------
@@ -383,6 +420,7 @@
 (defn tick! []
   (ensure-chrome!)
   (highlight-active-tab!)
+  (fetch-theme!)
   (mount-all-panes!)
   (-> (fetch-state!)
       (.then (fn [state]
